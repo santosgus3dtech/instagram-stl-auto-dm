76582774f1717de079b2f6fd637361825b8cb678
Gustavo Viana <guussvianna@gmail.com>
2026-08-26 15:41:50 -0300

    Remove personal follower audit tooling

--- stop_instagram_login_vnc.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-set KEY=%USERPROFILE%\.ssh\codex_raspberry_ed25519
-
-echo Parando navegador de login Meta/Facebook no Raspberry...
-ssh -i "%KEY%" -o IdentitiesOnly=yes codex@192.168.0.105 "sudo systemctl stop instagram-follow-login-vnc.service"
-echo Pronto. Voce tambem pode fechar a janela do tunel SSH.
-pause
